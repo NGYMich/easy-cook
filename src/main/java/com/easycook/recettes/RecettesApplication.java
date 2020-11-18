13110c4f6f284ae06f05de30c096bf3dcbf24b1e
NGYMich <ngmich95@gmail.com>
2020-11-18 15:02:19 +0100

    Premier commit

---
+++ src/main/java/com/easycook/recettes/RecettesApplication.java
@@ -0,0 +1,13 @@
+package com.easycook.recettes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecettesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecettesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easycook/recettes/RecettesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easycook.recettes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecettesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
