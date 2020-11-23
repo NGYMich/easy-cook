30db9aa8939b88268a79b300e3e6743ae8d68f79
NGYMich <ngmich95@gmail.com>
2020-11-24 00:04:43 +0100

    test

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://localhost:4200", "http://localhost:4200", "https://gourmich.herokuapp.com")
                 .allowedMethods("*")
                 .maxAge(3600L)
                 .allowedHeaders("*")
